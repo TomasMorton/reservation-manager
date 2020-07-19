58fdd4d1cf49989181eb8a77e6a3fca8515c7662
Tomas Morton <tomas.morton@broadsign.com>
2020-07-19 10:32:04 -0400

    Implement hard-coded in memory event store

--- src/EventStore.InMemory/Library.fs
+++ src/EventStore.InMemory/Library.fs
@@ -1,7 +1,18 @@
 ﻿namespace EventStore.InMemory
 
+open System
+open ReservationManager.Types
+
 module Events =
+    exception UnknownEntityType of string
+
+    let private getReservations () =
+        [ { Date = DateTime(2020, 01, 01)
+            NumberOfAds = 3 }
+          { Date = DateTime(2020, 02, 01)
+            NumberOfAds = 5 } ]
+
     let getEvents entityType =
         match entityType with
-        | "Reservations" -> ()
-        | _ -> ()
+        | "Reservations" -> getReservations ()
+        | _ -> raise (UnknownEntityType(sprintf "Unknown EntityType: %s" entityType))
