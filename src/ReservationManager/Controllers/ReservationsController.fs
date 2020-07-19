537df9019da9a2db22b54c76881699ee33bfc4d0
Tomas Morton <tomas.morton@broadsign.com>
2020-07-19 08:34:29 -0400

    Remove redundant "new"

--- src/ReservationManager/Controllers/ReservationsController.fs
+++ src/ReservationManager/Controllers/ReservationsController.fs
@@ -9,7 +9,7 @@ type ReservationsController() =
 
     [<HttpGet>]
     member __.GetReservations() =
-        [ { Date = new DateTime(2020, 01, 01)
+        [ { Date = DateTime(2020, 01, 01)
             NumberOfAds = 3 }
-          { Date = new DateTime(2020, 02, 01)
+          { Date = DateTime(2020, 02, 01)
             NumberOfAds = 5 } ]
