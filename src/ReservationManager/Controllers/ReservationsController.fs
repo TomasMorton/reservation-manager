0b7e3b5399be95e69dff97ab4e9bcf814a9879b6
Tomas Morton <tomas.morton@broadsign.com>
2020-07-19 08:33:47 -0400

    Return reservations from controller

--- src/ReservationManager/Controllers/ReservationsController.fs
+++ src/ReservationManager/Controllers/ReservationsController.fs
@@ -1,9 +1,15 @@
 namespace ReservationManager.Controllers
 
+open System
 open Microsoft.AspNetCore.Mvc
+open ReservationManager.Types
 
 type ReservationsController() =
     inherit BaseController()
 
     [<HttpGet>]
-    member __.GetReservations() = [ "reservation 1"; "reservation 2" ]
+    member __.GetReservations() =
+        [ { Date = new DateTime(2020, 01, 01)
+            NumberOfAds = 3 }
+          { Date = new DateTime(2020, 02, 01)
+            NumberOfAds = 5 } ]
