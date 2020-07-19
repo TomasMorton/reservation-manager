05b65c5895c4457c6b4565de42dd0048fc584311
Tomas Morton <tomas.morton@broadsign.com>
2020-07-19 14:30:16 -0400

    Clean up handler

--- src/ReservationManager.Web/Web.fs
+++ src/ReservationManager.Web/Web.fs
@@ -16,17 +16,24 @@ module HttpHandlers =
             let result = Queries.getAllReservations store
             negotiate result next ctx
 
+    let createCommandHandler () =
+        let state = []
+        CommandHandler.execute SystemClock.clock state
+
+    let createResponse result (next : HttpFunc) (ctx : HttpContext) =
+        match result with
+        | Result.Ok event ->
+            ctx.SetStatusCode 201
+            negotiate event next ctx
+        | Result.Error error ->
+            ctx.SetStatusCode 400
+            negotiate error next ctx
+
     let createReservation cmdData =
         fun (next : HttpFunc) (ctx : HttpContext) ->
-            let state = []
-            let handler = CommandHandler.execute SystemClock.clock state
-            match CreateReservation cmdData |> handler with
-            | Result.Ok event ->
-                ctx.SetStatusCode 201
-                negotiate event next ctx
-            | Result.Error error ->
-                ctx.SetStatusCode 400
-                negotiate error next ctx
+            let handler = createCommandHandler ()
+            let result = CreateReservation cmdData |> handler
+            createResponse result next ctx
 
 [<RequireQualifiedAccess>]
 module WebApp =
