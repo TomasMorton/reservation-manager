23da5a62aa775dadac10b510290e98ed1a9ac1c5
Tomas Morton <tomas.morton@broadsign.com>
2020-07-29 19:56:21 -0400

    Add sql store

---
+++ src/EventStore.Sql/SqlProviderDependencies/README.txt
@@ -0,0 +1,6 @@
+SQLProvider doesn't work well with netcore.
+https://fsprojects.github.io/SQLProvider/core/mssql.html is supposed to say how to use, but does not currently work.
+
+Files copied from:
+/.nuget/packages/microsoft.data.sqlclient/2.0.0/runtimes/unix/lib/netstandard2.0/Microsoft.Data.SqlClient.dll
+/.nuget/packages/microsoft.identity.client/4.14.0/lib/netstandard1.3/Microsoft.Identity.Client.dll
---
+++ src/EventStore.Sql/Store.fs
@@ -0,0 +1,17 @@
+﻿module EventStore.Sql.Store
+
+open FSharp.Data.Sql
+
+[<Literal>]
+let resolutionPath = __SOURCE_DIRECTORY__ + "/SqlProviderDependencies/"
+
+type sql =
+    SqlDataProvider<Common.DatabaseProviderTypes.MSSQLSERVER_DYNAMIC, "Server=localhost;Database=ReservationManager;User Id=sa;Password=Passw0rd", CaseSensitivityChange=Common.CaseSensitivityChange.ORIGINAL, UseOptionTypes=true, ResolutionPath=resolutionPath>
+
+let getEvents entityType =
+    let ctx = sql.GetDataContext()
+    query {
+        for aggregate in ctx.Dbo.Aggregates do
+            for event in aggregate.``dbo.Events by AggregateId`` do
+                select event
+    }
