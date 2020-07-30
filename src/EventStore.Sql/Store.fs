677c6ecbacd63afa428bd884522ca44026d93551
Tomas Morton <tomas.morton@broadsign.com>
2020-07-29 20:21:31 -0400

    Implement event parsing skeleton

--- src/EventStore.Sql/Store.fs
+++ src/EventStore.Sql/Store.fs
@@ -1,6 +1,9 @@
 ﻿module EventStore.Sql.Store
 
+open System
 open FSharp.Data.Sql
+open ReservationManager
+open ReservationManager.EventData
 
 [<Literal>]
 let resolutionPath = __SOURCE_DIRECTORY__ + "/SqlProviderDependencies/"
@@ -10,13 +13,29 @@ type sql =
         Common.DatabaseProviderTypes.MSSQLSERVER_DYNAMIC,
         "Server=localhost;Database=ReservationManager;User Id=sa;Password=Passw0rd",
         CaseSensitivityChange=Common.CaseSensitivityChange.ORIGINAL,
-        UseOptionTypes=true,
+        UseOptionTypes=false,
         ResolutionPath=resolutionPath>
+
+exception UnknownEntityType of string
+
+//todo
+let parseReservationCreatedData data : ReservationCreatedData =
+    { Date = DateTime.UtcNow
+      NumberOfAds = -1
+      ReservationId = ReservationId.create ()
+      Timestamp = DateTimeOffset.UtcNow }
+
+let toEvent (dto:sql.dataContext.``dbo.EventsEntity``) : Event =
+    match dto.Type with
+    | "ReservationCreated" -> parseReservationCreatedData dto.Data |> ReservationCreated
+    | _ -> raise (UnknownEntityType(sprintf "Unknown Event Type in Database: %s" dto.Type))
 
 let getEvents entityType =
     let ctx = sql.GetDataContext()
     query {
         for aggregate in ctx.Dbo.Aggregates do
             for event in aggregate.``dbo.Events by AggregateId`` do
+                where (aggregate.Type = entityType)
                 select event
-    }
+        
+    } |> Seq.map toEvent
